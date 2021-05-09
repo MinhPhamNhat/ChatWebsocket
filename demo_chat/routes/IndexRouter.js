d29696c0b105e043d96bc34fd7e5f82225a21023
PhamNhatMinh <73160254+MinhPhamNhat@users.noreply.github.com>
2021-05-09 16:55:34 +0700

    update

--- demo_chat/routes/IndexRouter.js
+++ demo_chat/routes/IndexRouter.js
@@ -7,7 +7,7 @@ const router = express.Router()
 const io = require("socket.io")()
 
 router.get("/", async(req, res, next) => {
-    let message_data = await Message.find()
+    let message_data = await Message.find().populate('userId').exec()
         // socket.join(req.user._json.name)
     res.render('index', { message_data: message_data, user: req.user })
 })
